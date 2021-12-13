b093b01121d3238f891b5079854784d18954c52c
Le Quang Duy <quangduynb00@gmail.com>
2021-12-13 15:52:28 +0700

    Font change

--- angular-using-bootstrap/src/app/app.module.ts
+++ angular-using-bootstrap/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    FontSizeEditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
